f112cf2f2dfe506456523784e5aca4fa3e9b08af
Kyle Hennessy <kyle.hennessy@tracktrain.com>
2015-11-29 21:15:56 -0800

    init radio toggle component

--- app/assets/javascripts/components/RadioSet.js.jsx
+++ app/assets/javascripts/components/RadioSet.js.jsx
@@ -8,19 +8,24 @@ class RadioSet extends React.Component{
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
-  handleClick(e){
-    e.preventDefault();
+  componentDidMount(){
+    $(React.findDOMNode(this)).find('input:checked').parent('label').addClass("active-radio");
+  }
+  handleClick(){
+    $(React.findDOMNode(this)).find('input:not(:checked)').parent('label').removeClass("active-radio");
+    $(React.findDOMNode(this)).find('input:checked').parent('label').addClass("active-radio");
+    this.props.callback(this.props.name, $(React.findDOMNode(this)).find('input:checked').val());
   }
   render(){
     return(
       <div className="radio hidden-radio">
         <label>
           {this.props.labels[0]}
-          <input type="radio" name={this.props.name} value="true" />
+          <input defaultChecked={this.props.checked == 'metric' ? true : false } type="radio" onClick={this.handleClick} name={this.props.name} value="first" />
         </label>
         <label>
           {this.props.labels[1]}
-          <input type="radio" name={this.props.name} value="false" />
+          <input defaultChecked={this.props.checked == 'metric' ? false : true } type="radio" onClick={this.handleClick} name={this.props.name} value="second" />
         </label>
       </div>
     );
--- app/assets/javascripts/components/Settings.js.jsx
+++ app/assets/javascripts/components/Settings.js.jsx
@@ -1,10 +1,26 @@
+// TODO features
+// user avatar
+// devise settings, email, change password
+// connect social accounts, twitter facebook
+
 class Settings extends React.Component{
   constructor(props){
     super(props);
-    this.weightRadios = this.weightRadios.bind(this);
+    this.radioChange = this.radioChange.bind(this);
   }
-  weightRadios(){
-
+  radioChange(unitName, checked){
+    let units = '';
+    switch (checked) {
+      case 'first':
+        units = 'metric';
+        break;
+      case 'second':
+        units = 'imperial';
+        break;
+      default:
+        console.log('default hit');
+    }
+    console.log(units);
   }
   render(){
     return(
@@ -14,25 +30,14 @@ class Settings extends React.Component{
             <legend>Units</legend>
             <div className="form-group">
               <label className="control-label">Girth Measurements</label>
-              <div className="c">
-                <div className="radio">
-                  <label>
-                    <input type="radio" name="girth" value="true" />
-                    Metric
-                  </label>
-                </div>
-                <div className="radio">
-                  <label>
-                    <input type="radio" name="girth" value="false" />
-                    US Standard
-                  </label>
-                </div>
+              <div className="col-xs-12">
+                <RadioSet checked='metric' callback={this.radioChange} name='girth_units' labels={['Metric', 'US Standard']} />
               </div>
             </div>
             <div className="form-group">
               <label className="control-label">Weight</label>
               <div className="col-xs-12">
-                <RadioSet callback={this.weightRadios} name='weight' labels={['Metric', 'US Standard']} />
+                <RadioSet checked='imperial' callback={this.radioChange} name='weight_units' labels={['Metric', 'US Standard']} />
               </div>
             </div>
           </fieldset>
